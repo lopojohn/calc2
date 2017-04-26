dae6474716e75a2b76dba38dd12a82a311e3ed22
john lopokoiyit <ljptios@gmail.com>
2017-04-26 09:47:50 +0300

    add javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter number:"));
+var number2 = parseInt(prompt("Enter other number:"));
+var result = add(number1, number2);
+alert(result);
