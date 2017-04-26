e519193665396578f421f97aed5905f9508e3697
john lopokoiyit <ljptios@gmail.com>
2017-04-26 10:20:18 +0300

    will add multiplication

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ var number1 = parseInt(prompt("Enter number:"));
 var number2 = parseInt(prompt("Enter other number:"));
 var result = add(number1, number2);
 alert(result);
+
+      var mul = function(number1, number2) {
+        return number1 * number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+
+      alert(mulS(number1, number2);
