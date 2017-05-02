ecb1925f198492e87867d8d263d74942c75ec83e
john lopokoiyit <ljptios@gmail.com>
2017-05-02 17:25:00 +0300

    make calculator with alert result

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
 
 
-var number1 = parseInt(prompt("Enter number:"));
-var number2 = parseInt(prompt("Enter other number:"));
+
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
-var result = add(number1, number2);
-alert(result);
+
+
 
 var mul = function(number1, number2) {
   return number1 * number2;
 };
-alert(mul(number1,number2));
+
+
+
+
+var subtract= function(number1,number2){
+  return number1 - number2;
+};
+
+
+var divide = function(number1,number2){
+  return number1 / number2;
+
+};
+
+
+
+
+      $("#add1").val();
+      $("#add2").val();
+      $(document).ready(function() {
+        $("form#add").submit(function(event) {
+          event.preventDefault();
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        var result = add(number1, number2);
+        alert(add(number1, number2));
+      });
+    });
